db70b81c8064c20dab3f9a8365eb2bfcbd9fcc61
vijay <vthapa@nksquare.co.in>
2020-04-11 00:07:36 +0530

    ToDo App first commit

---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/bootstrap.min.css')}}"  type="text/css" >
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<title>To Dos</title>
+</head>
+<body>
+<h1>TODOS PAGE</h1>
+
+
+
+</body>
+</html>
